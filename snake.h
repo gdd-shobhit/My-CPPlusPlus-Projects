a4b5d926ec7e090caa903754a81bb7202d4ff528
Shobhit Dhamania <sd5936@rit.edu>
2022-09-08 16:24:29 -0400

    Delete snake.h

--- snake.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Box2D/Box2D.h>
-void Wrapper();
-void Update(b2World* world);
-void applyForces(b2Body* snake);
-void moveTarget(float& xPos, float& yPos);
-void display(b2Vec2* target, b2Body* snake);
-void InitializeWallsObjects(b2World* world);
-float GetDistance(b2Vec2 pos1, b2Vec2 pos2);
